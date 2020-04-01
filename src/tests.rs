d6b380eab16050451fd3f2a4216904b7bc1f8e11
Brian Anderson <andersrb@gmail.com>
2020-04-01 02:02:32 -0600

    .

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,4 @@
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 use anyhow::Result;
 use std::io::Cursor;
 use serdif::Serializer;
@@ -12,14 +12,20 @@ fn test() -> Result<()> {
     let buf = buffer();
     let mut ser = Serializer::new(buf);
 
-    #[derive(Serialize)]
+    #[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
     struct Type1 {
         field1: bool,
         field2: bool,
     }
 
-    let val = Type1 { field1: true, field2: false };
-    val.serialize(&mut ser)?;
+    let val1 = Type1 { field1: true, field2: false };
+    val1.serialize(&mut ser)?;
+
+    let mut de = ser.to_de();
+
+    let val2 = Type1::deserialize(&mut de)?;
+
+    assert_eq!(val1, val2);
 
     Ok(())
 }
